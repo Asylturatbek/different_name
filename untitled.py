81e10caea78d5ae3d47af2219912e9850206dbe6
Asylturatbek <asylturatbek@gmail.com>
2019-10-30 15:29:58 +0600

    added some text to untitled

--- untitled.py
+++ untitled.py
@@ -1,5 +1,4 @@
-list = [1,2,3,5,2,3,34,6,3,1,32,63,45,34,22]
-for i in list.copy().items():
-	if i % 2 == 0 :
-		del list[i]
-print(i)
+I am writing these 
+in order to show what I can do 
+with git and other functions 
+using just my computer

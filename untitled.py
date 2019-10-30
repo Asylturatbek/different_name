409af32668274ffaacd75d24d5f6b809c7919d60
Asylturatbek <56810462+Asylturatbek@users.noreply.github.com>
2019-10-30 15:39:34 +0600

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -5,3 +5,13 @@ using just my computer
 
 
 again making other changes to my files OK?
+
+
+So let me be clear.. ok 
+If you want to be my girlfriend so bad, then
+that's okay Imma be your boyfriend then.
+But there are some conditions u know..
+like being as smart as I am and thing like this
+So there are big responsibilities for being my 
+girfriend. If you still wanna be that bad,
+then call this number: +923845797 :)
